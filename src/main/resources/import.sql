031392a1910680c3833e95e3f2aeaf6e8b2e638d
heimedina <heidimedina23@gmail.com>
2025-05-31 14:27:19 +0200

    updating data of bbdd

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,21 @@
 /*Populate tables */
-INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(0, 'Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
-INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(1, 'Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Andres', 'Lopez', 'profesional1@gmail.com', '2018-08-25');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Maria', 'Perez', 'profesional2@gmail.com', '2018-08-26');
