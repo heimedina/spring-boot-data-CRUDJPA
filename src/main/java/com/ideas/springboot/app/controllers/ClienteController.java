2dcc742b17d1415ebf970f38bc0897dfe679d7b1
heimedina <heidimedina23@gmail.com>
2025-06-13 11:46:38 +0200

    update Client Controller

--- src/main/java/com/ideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideas/springboot/app/controllers/ClienteController.java
@@ -5,7 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,6 +39,22 @@ public class ClienteController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping(value="/ver/{id}")
+	public String ver (@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+		
+		Cliente cliente = clienteService.findOne(id);
+		if(cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la Base de Datos");
+			return "redirect:/listar";
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle cliente: "+cliente.getNombre());
+		return "ver";
+	}
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
 	public String listar(@RequestParam(name="page", defaultValue = "0") int page,Model model) {
@@ -88,15 +108,17 @@ public class ClienteController {
 			return "form";
 		}
 		if(!foto.isEmpty()) {
-			Path directorioRecursos = Paths.get("src/main/resources/static/upload");
-			String roothPath = directorioRecursos.toFile().getAbsolutePath();
+			String uniqueFilename = UUID.randomUUID().toString()+"_" +foto.getOriginalFilename();
+			Path roothPath = Paths.get("upload").resolve(uniqueFilename);
+			Path rootAbsolutePath = roothPath.toAbsolutePath();
+			log.info("roothPath: "+roothPath);
+			log.info("rootAbsolutePath: "+rootAbsolutePath);
+			
 			try {
-				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(roothPath + "//" + foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes);
-				flash.addFlashAttribute("info", "Has subido correctamente '"+ foto.getOriginalFilename()+"'");
+				Files.copy(foto.getInputStream(), rootAbsolutePath);
+				flash.addFlashAttribute("info", "Has subido correctamente '"+ uniqueFilename+"'");
 				
-				cliente.setFoto(foto.getOriginalFilename());
+				cliente.setFoto(uniqueFilename);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
